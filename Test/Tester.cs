3d6d50ed93a7e0bb3e8a9763eb513b6a66dececd
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-10-21 15:14:59 +0200

    Update Tester.cs

--- Test/Tester.cs
+++ Test/Tester.cs
@@ -1,1 +1,34 @@
 
+using NUnit.Framework;
+using PrimeNumbers.PrimeHandler;
+
+namespace Tests
+{
+    public class TestANumberTests
+    {
+        TestANumber testANumber = new();
+        [Test]
+        [TestCase(int.MinValue, false)]
+        [TestCase(-1, false)]
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(10, false)]
+        [TestCase(100, false)]
+        [TestCase(1005, false)]
+        [TestCase(int.MaxValue - 1, false)]
+        public void PrimeTester__ExpectedFalse(int numberToTest, bool expected)
+        {
+            var actual = testANumber.PrimeCalculator(numberToTest);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        [TestCase(5, true)]
+        [TestCase(7, true)]
+        [TestCase(97, true)]
+        public void PrimeTester__ExpectedTrue(int numberToTest, bool expected)
+        {
+            var actual = testANumber.PrimeCalculator(numberToTest);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
